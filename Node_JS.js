0814d91126a8ee707549f63aea8652611b736230
Syed Haris Bukhari <45495476+HarisBukhari@users.noreply.github.com>
2022-07-04 23:54:13 +0500

    Update Node_JS.js

--- Node_JS.js
+++ Node_JS.js
@@ -153,7 +153,7 @@ router.post('/',upload.array('image'),(req,res)=>{
     console.log(req.body,req.files)
     res.send("Done")
 })
-
+ 
 //For Dev Enviroment ENV Locator
 if(process.env.NODE_ENV !=="production"){
     require('dotenv').config()
